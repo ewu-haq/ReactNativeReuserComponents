f078c814ed383b4f5bfcfc6cab06c36520721e7f
Quyen Ha <quyenha2511@gmail.com>
2017-12-11 07:27:08 -0800

    using new version of react native

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Quyen is here</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
